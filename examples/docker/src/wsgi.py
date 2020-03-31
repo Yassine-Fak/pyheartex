6e065f2c15422daaa3ca643b51a9a2081f6df1a6
gucharbon <gu.charbon@gmail.com>
2020-03-31 11:46:54 +0200

    fix missing image_dir argument

--- examples/docker/src/wsgi.py
+++ examples/docker/src/wsgi.py
@@ -6,13 +6,14 @@ logging.basicConfig(level=logging.INFO)
 
 from htx import app, init_model_server
 from image_classifier import FastaiImageClassifier, train_script
-from settings import REDIS_HOST, REDIS_PORT, RQ_QUEUE_NAME, MODEL_DIR
+from settings import REDIS_HOST, REDIS_PORT, RQ_QUEUE_NAME, MODEL_DIR, IMAGE_DIR
 
 
 init_model_server(
     create_model_func=FastaiImageClassifier,
     train_script=train_script,
     num_iter=10,
+    image_dir=IMAGE_DIR,
     model_dir=MODEL_DIR,
     redis_queue=RQ_QUEUE_NAME,
     redis_host=REDIS_HOST,
