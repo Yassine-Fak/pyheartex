10176a843e065a7df77bd0e6a350c6b3bb2265e0
gucharbon <gu.charbon@gmail.com>
2020-03-31 10:26:55 +0200

    fix docker example

--- examples/docker/src/wsgi.py
+++ examples/docker/src/wsgi.py
@@ -6,14 +6,14 @@ logging.basicConfig(level=logging.INFO)
 
 from htx import app, init_model_server
 from image_classifier import FastaiImageClassifier, train_script
-from settings import REDIS_HOST, REDIS_PORT, RQ_QUEUE_NAME
+from settings import REDIS_HOST, REDIS_PORT, RQ_QUEUE_NAME, MODEL_DIR
 
 
 init_model_server(
     create_model_func=FastaiImageClassifier,
     train_script=train_script,
     num_iter=10,
-    image_dir='~/.heartex/images',
+    model_dir=MODEL_DIR,
     redis_queue=RQ_QUEUE_NAME,
     redis_host=REDIS_HOST,
     redis_port=REDIS_PORT,
