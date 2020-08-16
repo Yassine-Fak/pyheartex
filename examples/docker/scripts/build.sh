c136d1b0cf83aff5cc37d3609c1b84dac7d53c6d
Yassine Fakihani <yassine.fakihani@capgemini.com>
2020-08-16 22:19:58 +0200

    adiing label studio image

--- examples/docker/scripts/build.sh
+++ examples/docker/scripts/build.sh
@@ -2,3 +2,4 @@
 
 docker build -t gcharbon/pyheartex-example-server -f Dockerfile .
 docker build -t gcharbon/pyheartex-example-worker -f Dockerfile.worker .
+# docker build -t label-studio -f Dockerfile.label-studio .
